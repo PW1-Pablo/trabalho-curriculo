e223dcaf1d704e1d70c37b3adf20b1eeec426f46
PABLO ONOFRE <Gmod9K>
2022-09-29 23:45:56 -0300

    "init"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import CurriculumForm from "./components/Form"
+import { curriculumService } from "./services"
+import CurriculumFormat from "./components/Format"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+  const [data, setData] = useState([])
+
+
+  const [editing, setEditing] = useState(false)
+
+  useEffect(() => {
+    const data = curriculumService.getData()
+    setData(data)
+  }, [])
+
+  const updateForm = (form) => {
+    const newData = [...data, form]
+
+    setData(newData)
+    curriculumService.saveData(newData)
+    setEditing(false)
+  }
+
+  const editingTrue = () => {
+    setEditing(true)
+  }
+
+  console.log(data.name)
+
+  if(!editing && data == null) {
+    return(
+      <input type="button" value="Criar Curriculo" onClick={editingTrue} />
+    )
+  } else if (editing) {
+      return(
+        <div>
+          <CurriculumForm onSave={updateForm} />
+        </div>
+      )
+  } 
+  
+  if (data != null) {
+    return(
+      <div>
+        <CurriculumFormat data={data} />
+      </div>
+    )
+  }
+  
 }
 
 export default App;
